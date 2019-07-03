4830052007db04b5b8c1b2e3fad29e2f5564768d
armagedon007 <000yurik000@gmail.com>
2019-07-04 02:31:30 +0700

    Update web.php

--- config/web.php
+++ config/web.php
@@ -7,6 +7,6 @@ return [
         'class' => 'app\db\Connection',
         'dsn' => 'mysql:host=localhost;port=3306;dbname=guestbook',
         'username' => 'root',
-        'password' => '123456',
+        'password' => '1234567',
     ]
 ];
